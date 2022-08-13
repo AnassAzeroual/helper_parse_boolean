b456df6212cc276f7744e378869d98df5cac0b45
AnassAzeroual <azeroual.anass2015@gmail.com>
2022-08-13 14:58:39 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -5,38 +5,53 @@ import './style.css';
 const appDiv: HTMLElement = document.getElementById('app');
 let output = '';
 let list = [
-  '',
-  ' ',
-  ' _',
   'true',
+  true,
   'false',
+  false,
   'True',
   'False',
   'TRUE',
   'FALSE',
-  'trues',
-  'falses',
+  'truess',
+  'falsess',
   'testo',
+  '',
+  ' ',
+  ' _',
   '[]',
+  [],
   '{}',
+  {},
   '??',
   '!!',
+  'null',
+  null,
+  'undefined',
+  undefined,
 ];
 
-function parserboolean(ipt: string | boolean):boolean | undefined {
+function parserboolean(ipt: string | boolean): boolean | undefined {
   if (['true', 'false'].includes(String(ipt).toLowerCase())) {
     return JSON.parse(String(ipt).toLowerCase());
   }
 }
 function loopi() {
-  console.clear()
+  console.clear();
   for (let i = 0; i < list.length; i++) {
-    console.log(`${list[i]} => ${typeof parserboolean(list[i])}`);
+    var tag = document.createElement('h1');
+    var text = document.createTextNode(
+      `typeof(${typeof list[i]}) value(${list[i]})  ==> result(${parserboolean(
+        list[i]
+      )})`
+    );
+    console.log(`${list[i]} => ${parserboolean(list[i])}`);
+    tag.appendChild(text);
+    appDiv.appendChild(tag);
   }
 }
 
+loopi();
+// output = parserboolean('False');
 
-loopi()
-output = parserboolean('False');
-
-appDiv.innerHTML = `<h1>${output}</h1>`;
+// appDiv.innerHTML = `<h1>${output}</h1>`;
